a539072075ae59938d95b2df6f87a5095ecdf73e
tbharvey <timothybharvey@gmail.com>
2017-03-11 11:22:20 -0500

    201703111122

--- Excercise_2.R
+++ Excercise_2.R
@@ -11,7 +11,7 @@ ex2_frame <- read.csv("titanic_original.csv")
 # the missing values and replace them with S. (Caution: Sometimes a missing
 # value might be read into R as a blank or empty string.)
 
-ex2_frame <- exsub( "^$", "S", ex2_frame$embarked)
+ex2_frame$embarked <- sub( "^$", "S", ex2_frame$embarked)
 
 # 2: Age You’ll notice that a lot of the values in the Age column are missing.
 # While there are many ways to fill these missing values, using the mean or
@@ -22,19 +22,24 @@ ex2_frame <- exsub( "^$", "S", ex2_frame$embarked)
 
 ex2_frame$age[which(is.na(ex2_frame$age))] <- mean(ex2_frame$age, na.rm = TRUE)
 
-
-View(ex2_frame)
 # 3: Lifeboat You’re interested in looking at the distribution of passengers in
 # different lifeboats, but as we know, many passengers did not make it to a boat
 # :-( This means that there are a lot of missing values in the boat column. Fill
 # these empty slots with a dummy value e.g. the string 'None' or 'NA'
 
+ex2_frame$boat <- sub("^$", NA_character_, ex2_frame$boat)
+
 # 4: Cabin You notice that many passengers don’t have a cabin number associated
 # with them. Does it make sense to fill missing cabin numbers with a value?
+
+#Yes, as it allows you to utilise the is.na function. Generally better to not have blank fields.
+ex2_frame$cabin <- sub("^$", NA_character_, ex2_frame$cabin)
 
 # What does a missing value here mean? You have a hunch that the fact that the
 # cabin number is missing might be a useful indicator of survival. Create a new
 # column has_cabin_number which has 1 if there is a cabin number, and 0
 # otherwise.
 
+ex2_frame$has_cabin_number <- ifelse(is.na(ex2_frame$cabin), "0", "1")
 
+View(ex2_frame)
